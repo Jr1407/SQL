2b8d684aded0723ff0c9c2a8464e96b0092b47ba
Jishnu Ray <121662868+Jr1407@users.noreply.github.com>
2025-12-18 00:10:58 +0530

    Modify main.py
    Removed evaluation step and unused test data.

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 # main.py
-
 from src.dataset import load_text2sql_dataset
 from src.model import load_model_and_tokenizer
 from src.train import train_model
-from src.evaluate import evaluate_model
 
 
 def main():
-    train_data, test_data = load_text2sql_dataset()
+    train_data, _ = load_text2sql_dataset()
     tokenizer, model = load_model_and_tokenizer()
-    train_model(model, tokenizer, train_data)
-    evaluate_model(model, tokenizer, test_data)
+    train_model(model, train_data)
 
 
 if __name__ == "__main__":
     main()
+
